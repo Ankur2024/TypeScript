3c12985058f433415c1626990fd52a1ac3a9449e
Ankur2024 <ag3942280@gmail.com>
2025-05-08 08:12:49 +0530

    Revising Typesript

---
+++ TYPESCRIPT/app.ts
@@ -0,0 +1,86 @@
+// var a = 12;
+// console.log(a)
+
+// //tuple
+// let arr: [string, number] = ["Ankur", 1]
+// let arr1: [number, boolean] = [4, true]
+
+// //enum
+// enum UserRoles {
+//     ADMIN = "admin",
+//     USER = "user"
+// }
+
+// enum StatusCode {
+//     OK = 200,
+//     Created = 201,
+//     BadRequest = 400,
+//     Unauthorized = 401,
+//     Forbidden = 403,
+//     NotFound = 404,
+//     InternalServerError = 500
+// }
+
+
+// interface
+// interface User {
+//     name: string,
+//     age: number,
+//     isActive: boolean
+// }
+
+// function getData(user: User) {
+//     console.log(user)
+// }
+
+// let user: User = {
+//     name: "Ankur",
+//     age: 23,
+//     isActive: true
+// }
+
+// getData(user)
+
+// interface Admin extends User {
+//     role?: string
+// }
+
+// let admin: Admin = {
+//     name: "Ankur",
+//     age: 23,
+//     isActive: true,
+//     role: "Admin"
+// }
+// getData(admin)
+
+
+// Intersection Types
+
+// type arg = String | number | null
+// function obj(val: arg) {
+// }
+
+// type intersectionUser = {
+//     name: string,
+//     email: string,
+// }
+
+// type Admin_1 = intersectionUser & {
+//     getDetail(user: string): void
+// }
+
+// function abcs(a: Admin_1){
+//     console.log(a)
+// }
+
+//Class & Object
+
+class Device{
+    name = "lg";
+    price = 20000;
+    category = "digital";
+}
+
+let d1 = new Device();
+let d2 = new Device();
+
